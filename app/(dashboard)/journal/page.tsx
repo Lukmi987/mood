c75b3655ce479ee2f66af13f5c5c13d24ad919ff
Lukas Komprs <lukas.komprs@ais-servis.cz>
2025-01-02 14:10:18 +0100

    OPEN AI setup, prompt template, structured output with Zod, creating consistent prompts

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,6 @@
 import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
+import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import Link from 'next/link'
@@ -15,12 +16,16 @@ const getEntries = async () => {
     },
   })
 
+  await analyze(
+    `Today was a eh, ok day I guess. I found a new coffee shop that was cool but hen i got a flat tire.`
+  )
+
   return entries
 }
 
 const JournalPage = async () => {
   const entries = await getEntries()
-  console.log('entry je', entries)
+  // console.log('entry je', entries)
   return (
     <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
