e8411b1661f505f28ea89b3aa8a27ca84fe91088
Lukas Komprs <lukas.komprs@ais-servis.cz>
2025-01-15 15:55:13 +0100

    revalidation client state management

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -12,6 +12,10 @@ const getEntry = async (id) => {
         id,
       },
     },
+    include: {
+      // it is like JOIN table in SQL
+      analysis: true,
+    },
   })
 
   return entry
@@ -20,38 +24,12 @@ const getEntry = async (id) => {
 // you can pass props to client components from a server component, it just has to be serializable(if it is save in db it is 100%)
 const EntryPage = async ({ params }) => {
   // .id because of the name of the folder [id]
-  const entry = await getEntry(params.id)
-
-  const analysisData = [
-    { name: 'Summary', value: '' },
-    { name: 'Subject', value: '' },
-    { name: 'Mood', value: '' },
-    { name: 'Negative', value: '' },
-  ]
+  const { id } = await params
+  const entry = await getEntry(id)
 
   return (
-    <div className="h-full w-full grid grid-cols-3 gap-4">
-      <div className="col-span-2">
-        <Editor entry={entry} />
-      </div>
-      <div className="border-l border-black/10">
-        <div className="bg-blue-300 px-6 py-10">
-          <h2 className="text-2xl">Analysis</h2>
-        </div>
-        <div>
-          <ul>
-            {analysisData.map((item) => (
-              <li
-                key={item.name}
-                className=" px-2 py-4  border-t border-black/10 flex items-center justify-between"
-              >
-                <span className="text-lg font-semibold ">{item.name}</span>
-                <span>{item.value}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div className="h-full w-full">
+      <Editor entry={entry} />
     </div>
   )
 }
--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
@@ -5,19 +6,42 @@ import { NextResponse } from 'next/server'
 // Patch(update) is update som props in DB, and PUT(replace) override the whole item in db
 export const PATCH = async (request, { params }) => {
   const { content } = await request.json()
+  console.log('use content 2', content)
   const user = await getUserByClerkID()
+  const { id } = await params
   const updatedEntry = await prisma.journalEntry.update({
     where: {
       userId_id: {
         userId: user.id,
-        id: params.id, // params.id coz .id coz the name of folder api/journal/[id]
+        id: id, // params.id coz .id coz the name of folder api/journal/[id]
       },
     },
     data: {
       //data what you want to update
       content,
     },
+    // include: { Now it would inlcude analysis in response
+    //   analysis: true,
+    // },
   })
 
-  return NextResponse.json({ data: updatedEntry })
+  console.log('use content updated 3', updatedEntry)
+  const analysis = await analyze(updatedEntry.content) // spreading the object that is returned
+
+  const updated = await prisma.analysis.upsert({
+    // upsert means updated if you find it
+    where: {
+      entryId: updatedEntry.id,
+    }, //data what you want to update
+    create: {
+      entryId: updatedEntry.id,
+      ...analysis,
+    },
+    update: analysis,
+  })
+
+  console.log('use content 4', updated)
+  const x = NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
+  console.log('use c ontent 5', x)
+  return x
 }
